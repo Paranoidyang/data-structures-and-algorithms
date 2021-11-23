5e1388af0821ac829d53da10b5e485b0e68f3c0d
行走的仆累 <paranoidyang@163.com>
2021-11-23 23:26:01 +0800

    手写new操作符

---
+++ src/core/手写js系列/手写new操作符.js
@@ -0,0 +1,34 @@
+/**
+ * 手写new操作符
+ * 使用构造函数，new一个实例的过程发生了一下过程：
+          （1）在内存中创建一个新对象。
+          （2）这个新对象内部的[[Prototype]]特性被赋值为构造函数的prototype属性。
+          （3）构造函数内部的this指向这个新对象
+          （4）执行构造函数内部的代码（给新对象添加属性）。
+          （5）如果构造函数显式地返回一个非空对象，则返回该对象；否则，返回刚创建的新对象。
+ */
+export default function() {
+  function myNew(fn, ...args) {
+    // 方法1
+    // let obj = object.create(fn.prototype)
+    // 方法2
+    let obj = {}
+    // 将新对象obj关联构造函数的原型
+    obj.__proto__ = fn.prototype
+    // 为obj添加属性
+    let result = fn.call(obj, ...args)
+    if(typeof result === 'object' || typeof result === 'function') {
+      return result
+    }else {
+      return obj
+    }
+  }
+
+  function Person(name, age) {
+    this.name = name
+    this.age = age
+    // return {a: 1}
+  }
+  console.log(myNew(Person, '小红', 18))
+
+}
--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ import App from './App.vue'
 // import test from './core/手写js系列/递归方式扁平化数组'
 // import test from './core/手写js系列/简易版Promise.js'
 // import test from './core/手写js系列/完整版Promise.js'
-import test from './core/手写js系列/手写防抖、节流函数.js'
+// import test from './core/手写js系列/手写防抖、节流函数.js'
+import test from './core/手写js系列/手写new操作符'
 test()
 
 
