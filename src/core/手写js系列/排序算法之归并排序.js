8b8d8301706738397870b71c48fe1b8ebd6985ad
行走的仆累 <paranoidyang@163.com>
2021-12-23 22:14:19 +0800

    完善归并排序

--- src/core/手写js系列/排序算法之归并排序.js
+++ src/core/手写js系列/排序算法之归并排序.js
@@ -5,23 +5,23 @@
 export default function () {
   // 辅助函数：合并和排序小数组，产生大数组
   function merge(left, right) {
-    const resArr = []
+    const resArr = [];
     while (left.length > 0 && right.length > 0) {
-      resArr.push(left[0] <= right[0] ? left.shift() : right.shift())
+      // 循环比较两个小数组的第一个元素，将小的放进数组里，直到两个数组有一个为空
+      resArr.push(left[0] <= right[0] ? left.shift() : right.shift());
     }
-    return resArr.concat(left, right);
+    return resArr.concat(left, right); //有一个数组为空，就直接将另一个数组拼到resArr的后面
   }
 
   //将一个大数组分为多个小数组，并调用用来排序的辅助函数
   function mergeSort(arr) {
-    let length = arr.length
-    if (length < 2) return arr
-    const mid = Math.floor(length / 2)
-    const left = mergeSort(arr.slice(0, mid))
-    const right = mergeSort(arr.slice(mid))
-    return merge(left, right)
+    let length = arr.length;
+    if (length < 2) return arr;
+    const mid = Math.floor(length / 2);
+    const left = mergeSort(arr.slice(0, mid));
+    const right = mergeSort(arr.slice(mid));
+    return merge(left, right);
   }
 
-  console.log(mergeSort([2, 3, 4, 1, 4, 1, 5]))
-
+  console.log(mergeSort([2, 3, 4, 1, 4, 1, 5]));
 }
