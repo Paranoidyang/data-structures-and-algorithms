09a871c6a32f734bb9fc95d1fe9bd9240007d76d
paranoidyang <paranoidyang@163.com>
2022-05-18 16:43:24 +0800

    删除链表重复元素

--- src/core/数据结构与算法/链表/删除链表重复元素.js
+++ src/core/数据结构与算法/链表/删除链表重复元素.js
@@ -1,7 +1,12 @@
 /**
- * 题目描述：将两个有序链表合并为一个新的有序链表并返回。新链表是通过拼接给定的两个链表的所有结点组成的。
- * 示例：输入：1->2->4, 1->3->4 输出：1->1->2->3->4->4 
- * 思路：做链表处理类问题，大家要把握住一个中心思想——处理链表的本质，是处理链表结点之间的指针关系。
+ * 题目描述：给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。
+ * 示例：
+ * 输入: 1->1->2
+   输出: 1->2
+   示例 2:
+   输入: 1->1->2->3->3
+   输出: 1->2->3
+ * 思路：将需要删除的目标结点的前驱结点 next 指针往后指一格。判断两个元素是否重复，由于此处是已排序的链表，我们直接判断前后两个元素值是否相等即可。
  */
 
 /**
@@ -11,29 +16,21 @@
  *     this.next = null;
  * }
  */
-function ListNode(val) {
-   this.val = val;
-   this.next = null;
-}
+
 /**
- * 有序链表的合并
- * @param {*} l1 链表1
- * @param {*} l2 链表2
+ * 删除有序链表的重复元素
+ * @param {*} head 
  */
-function mergeTwoLists(l1, l2) {
-   let head = new ListNode()// 定义头结点，确保链表可以被访问到
-   let cur = head// cur 这里就是咱们那根“针”
-   while (l1 && l2) {
-      if (l1.val <= l2.val) { // 如果 l1 的结点值较小
-         cur.next = l1 // 先串起 l1 的结点
-         l1 = l1.next// l1 指针向前一步
+function deleteDuplicates(head) {
+   let cur = head// 设定 cur 指针，初始位置为链表第一个结点
+   while (cur != null && cur.next != null) {
+      let next = cur.next
+      if (cur.val === next.val) {// 若当前结点和它后面一个结点值相等（重复）
+         cur.next = next.next // 删除靠后的那个结点（去重）
       } else {
-         cur.next = l2 // l2 较小时，串起 l2 结点
-         l2 = l2.next// l2 向前一步
+         cur = cur.next// 若不重复，继续遍历
       }
-      cur = cur.next// “针”在串起一个结点后，也会往前一步
    }
-   cur.next = l1 !== null ? l1 : l2// 处理链表不等长的情况，因为是有序的，直接串起来即可
-   return head.next// 返回起始结点
+   return head
 }
 
