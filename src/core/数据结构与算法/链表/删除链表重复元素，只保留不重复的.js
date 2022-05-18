a8428e2f98450c39b580e62bf0cfdef2b8332c46
paranoidyang <paranoidyang@163.com>
2022-05-18 18:33:01 +0800

    删除链表重复节点

---
+++ src/core/数据结构与算法/链表/删除链表重复元素，只保留不重复的.js
@@ -0,0 +1,28 @@
+/**
+ * 题目描述：给定一个排序链表，删除所有含有重复数字的结点，只保留原始链表中 没有重复出现的数字。
+ * 示例：
+ * 输入: 1->2->3->3->4->4->5
+   输出: 1->2->5
+   示例 2:
+   输入: 1->1->1->2->3
+   输出: 2->3
+ */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+function ListNode(val) {
+   this.val = val;
+   this.next = null;
+}
+/**
+ * 删除有序链表的所有重复元素，只保留不重复的节点
+ * @param {*} head 
+ */
+function deleteDuplicates(head) {
+}
+
