c65341ccfc797993499ca9dde5ffb3834ff75e34
行走的仆累 <paranoidyang@163.com>
2022-05-21 00:10:46 +0800

    最小栈问题

--- src/core/数据结构与算法/栈/包含min函数的栈.js
+++ src/core/数据结构与算法/栈/包含min函数的栈.js
@@ -1,52 +1,50 @@
 /**
- * 题目描述：定义栈的数据结构，请在该类型中实现一个能够得到栈的最小元素的 min 函数在该栈中，调用 min、push 及 pop 的时间复杂度都是 O(1)。
+ * 题目描述：设计一个支持 push ，pop ，top 操作，并能在常数时间内检索到最小元素的栈。
+ * pop() —— 删除栈顶的元素。
+ * top() —— 获取栈顶元素。
+ * getMin() —— 检索栈中的最小元素。
  */
 
 /**
  * 解题分析：使用两个数组实现，一个数组保存存入的数据，另一个数组始终存入当前最小值
  */
 
-export default function() {
+function MinStack() {
+  this.arr = []
+  this.minArr = []
+}
 
-  function MinStack() {
-    this.arr = []
-    this.minArr = []
+MinStack.prototype.push = function (x) {
+  this.arr.push(x)
+  if (this.minArr.length === 0 || x <= this.minArr[this.minArr.length - 1]) {//当栈为空或者x小于栈顶元素时，入栈
+    this.minArr.push(x)
   }
-  
-  MinStack.prototype.push = function(x) {
-    this.arr.push(x)
-    if(this.minArr.length === 0) {
-      this.minArr.push(x)
-    }else {
-      //保证栈顶始终是最小值
-      let currentMin = this.minArr[this.minArr.length - 1]
-      if(x > currentMin) {
-        this.minArr.push(currentMin)
-      }else {
-        this.minArr.push(x)
-      }
-    }
-  }
-  
-  MinStack.prototype.pop = function() {
-    this.arr.pop()
+}
+
+MinStack.prototype.pop = function () {
+  let top = this.arr.pop()
+  if (top === this.minArr[this.minArr.length - 1]) {
     this.minArr.pop()
   }
-  
-  MinStack.prototype.min = function() {
-    console.log('stack', this.arr)
-    console.log('minStack', this.minArr)
-    return this.minArr[this.minArr.length -1]
-  }
-
-  let minStack = new MinStack()
-  minStack.push(-2)
-  minStack.push(0)
-  minStack.push(0)
-  let min = minStack.min()
-  console.log('min', min)
-  
-  
 }
+
+MinStack.prototype.top = function () {
+  return this.arr[this.arr.length - 1]
+}
+
+MinStack.prototype.getMin = function () {
+  console.log('stack', this.arr)
+  console.log('minStack', this.minArr)
+  return this.minArr[this.minArr.length - 1]
+}
+
+let minStack = new MinStack()
+minStack.push(0)
+minStack.push(0)
+minStack.push(1)
+minStack.push(-2)
+minStack.pop()
+console.log('top', minStack.top())
+console.log('getMin', minStack.getMin())
 
 
