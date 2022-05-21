334e7dcb9e4da53eaff4c2f6747d50c6b6732184
行走的仆累 <paranoidyang@163.com>
2022-05-21 15:48:05 +0800

    用两个栈实现队列

--- src/core/数据结构与算法/队列/用两个栈实现队列.js
+++ src/core/数据结构与算法/队列/用两个栈实现队列.js
@@ -1,51 +1,57 @@
 /**
- * 题目描述：用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ * 题目描述：请你仅使用两个栈实现先入先出队列。队列应当支持一般队列支持的所有操作（push、pop、peek、empty）：
+ * 实现 MyQueue 类：
+    void push(int x) 将元素 x 推到队列的末尾
+    int pop() 从队列的开头移除并返回元素
+    int peek() 返回队列开头的元素
+    boolean empty() 如果队列为空，返回 true ；否则，返回 false
  */
 
 /**
  * 解题分析：一个栈实现入队列，另一个栈实现出队列
  */
 
-import Stack from '@/base/stack'
-export default function() {
-
-  function CQueue() {
-    this.stack1 = new Stack()
-    this.stack2 = new Stack()
-  };
-  
-  CQueue.prototype.appendTail = function(value) {
-    this.stack1.push(value)
-  };
-  
-  CQueue.prototype.deleteHead = function() {
-    //空队列，返回-1
-    if(this.stack1.size() === 0) {
-        return -1
-    }
-    //只要stack1有值，就将其插入stack2，方便弹出，模拟出队列
-    while(this.stack1.size() > 0) {
-        this.stack2.push(this.stack1.pop())
-    }
-    let head = this.stack2.pop()
-  
-    //出队列后，重新放回stack1
-    while(this.stack2.size() > 0) {
-        this.stack1.push(this.stack2.pop())
-    }
-    return head
-  };
-
-  CQueue.prototype.size = function() {
-    return this.stack1.size()
+class myQueue {
+  constructor() {
+    this.stack1 = []// 初始化两个栈
+    this.stack2 = []
   }
-  
-  let myQueue = new CQueue()
-  myQueue.appendTail(1)
-  myQueue.appendTail(2)
-  console.log('myQueue', myQueue, myQueue.size())
-  let delItem = myQueue.deleteHead()
-  console.log('del元素', delItem)
-  console.log('删除后后size', myQueue.size())
+  // 队尾添加元素
+  push(x) {
+    this.stack1.push(x)
+  }
+  // 队头删除元素
+  pop() {
+    if (this.stack2.length <= 0) {
+      while (this.stack1.length > 0) {//注意此操作后 stack1 为空，数据都到 stack2 中
+        this.stack2.push(this.stack1.pop())
+      }
+    }
+    return this.stack2.pop()// 为了达到逆序的目的，我们只从 stack2 里出栈元素
+  }
+  // 返回队头元素
+  peek() {
+    if (this.stack2.length <= 0) {
+      while (this.stack1.length > 0) {//注意此操作后 stack1 为空，数据都到stack2中
+        this.stack2.push(this.stack1.pop())
+      }
+    }
+    const stack2Len = this.stack2.length// 缓存 stack2 的长度
+    return stack2Len ? this.stack2[stack2Len - 1] : -1
+  }
+  // 队列是否为空
+  empty() {
+    return this.stack1.length === 0 && this.stack2.length === 0// 若 stack1 和 stack2 均为空，那么队列空
+  }
 }
 
+let queue = new myQueue()
+queue.push(1)
+queue.push(2)
+queue.push(3)
+queue.push(4)
+queue.pop()
+queue.pop()
+queue.pop()
+console.log('peek', queue.peek())
+console.log('empty', queue.empty())
