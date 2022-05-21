df7430fcbb6ef8a88a232ef68c2d60e3ae54b0db
行走的仆累 <paranoidyang@163.com>
2022-05-21 16:44:54 +0800

    添加注释

--- src/core/数据结构与算法/队列/滑动窗口问题—双端队列法.js
+++ src/core/数据结构与算法/队列/滑动窗口问题—双端队列法.js
@@ -11,19 +11,21 @@ function maxSlidingWindow(nums, k) {
   const len = nums.length
   let left = 0
   let right = k
-  const res = []
-  while (right <= len) {
-    let arr = nums.slice(left, right)
+  const res = [] // 定义结果数组
+  while (right <= len) {// right等于len说明遍历完
+    let arr = nums.slice(left, right)//截取滑动窗口内的元素
     console.log(arr)
-    let max = arr[0]
-    for (let i = 1; i < arr.length; i++) {
-      max = arr[i] > max ? arr[i] : max
-    }
-    res.push(max)
-    left++
-    right++
+    // let max = arr[0]
+    // for (let i = 1; i < arr.length; i++) {//找出窗口内的最大值
+    //   max = arr[i] > max ? arr[i] : max
+    // }
+    max = Math.max(...arr)// 上面的循环可以用Math.max代替
+    res.push(max)// 将最大值推入结果数组
+    left++ // 左指针前进一步
+    right++// 右指针前进一步
   }
   return res
 }
 
 console.log(maxSlidingWindow([1, 3, -1, -3, 5, 3, 6, 7], 3))
+
