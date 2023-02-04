4300bdcd1edcb1bee8157de1dfa4045fee2530d4
paranoidyang <paranoidyang@163.com>
2023-02-05 00:07:48 +0800

    删除字符串中的所有相邻重复项

---
+++ src/core/数据结构与算法/栈和队列/删除字符串中的所有相邻重复项.js
@@ -0,0 +1,33 @@
+/**
+ * 题目描述：给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。在 S 上反复执行重复项删除操作，直到无法继续删除。在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
+ * 
+ * 示例：
+ *  输入："abbaca"
+    输出："ca"
+    解释：例如，在 "abbaca" 中，我们可以删除 "bb" 由于两字母相邻且相同，这是此时唯一可以执行删除操作的重复项。之后我们得到字符串 "aaca"，其中又只有 "aa" 可以执行重复项删除操作，所以最后的字符串为 "ca"。
+
+   提示：
+    1 <= S.length <= 20000
+    S 仅由小写英文字母组成。
+ */
+
+/**
+ * 注意：删除两个相邻且相同的字母，所以是成双成对的删除，当有三个相同字母时，只需删除其中两个
+ * @param {*} s 
+ * @returns 
+ */
+function removeDuplicates(s) {
+  const stack = []
+  for (const x of s) {
+    const len = stack.length
+    if (len && stack[len - 1] === x) {
+      stack.pop() // 已入栈的弹出，并continue就可以排除一对相邻且相等的字母
+      continue
+    }
+    stack.push(x)
+  }
+  return stack.join('')
+}
+
+// 测试
+console.log(removeDuplicates('abbaca'))
