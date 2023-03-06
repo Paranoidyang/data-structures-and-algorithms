292f410fdda9cc108efb4b8dc890e86228341086
paranoidyang <paranoidyang@163.com>
2023-03-06 23:23:17 +0800

    递归判断是否为对称二叉树

--- src/core/数据结构与算法/二叉树/对称二叉树.js
+++ src/core/数据结构与算法/二叉树/对称二叉树.js
@@ -1,3 +1,31 @@
 /**
  * 题目描述：给定一个二叉树，检查它是否是镜像对称的。
  */
+
+/**
+ * 递归法
+ * @param {*} root 
+ * @returns 
+ */
+ function isSymmetric(root) {
+  // 使用递归遍历左右子树 递归三部曲
+    // 1. 确定递归的参数 root.left root.right和返回值true false 
+    const compareNode = function(left, right) {
+      // 2. 确定终止条件 空的情况
+      if(left === null && right !== null || left !== null && right === null) {
+          return false;
+      } else if(left === null && right === null) {
+          return true;
+      } else if(left.val !== right.val) {
+          return false;
+      }
+      // 3. 确定单层递归逻辑
+      let outSide = compareNode(left.left, right.right);
+      let inSide = compareNode(left.right, right.left);
+      return outSide && inSide;
+  }
+  if(root === null) {
+      return true;
+  }
+  return compareNode(root.left, root.right);
+ } 
