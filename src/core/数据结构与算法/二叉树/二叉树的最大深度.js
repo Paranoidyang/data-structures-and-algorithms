ac0bf8667ab2080936b6e7b15330691cfe8ae1c5
paranoidyang <paranoidyang@163.com>
2023-03-08 23:51:38 +0800

    递归法求二叉树的最大深度

---
+++ src/core/数据结构与算法/二叉树/二叉树的最大深度.js
@@ -0,0 +1,50 @@
+/**
+ * 题目描述：给定一个二叉树，找出其最大深度。
+ * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+ * 说明: 叶子节点是指没有子节点的节点。
+ * 示例： 给定二叉树 [3,9,20,null,null,15,7]，返回它的最大深度 3 。
+ */
+const root = {
+  val: "0",
+  left: {
+    val: "1",
+    left: {
+      val: "2",
+      left: null,
+      right: null,
+    },
+    right: {
+      val: "3",
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    val: "1",
+    left: null,
+    right: null,
+  },
+};
+/**
+ * 递归法（后序遍历求高度，根节点的高度就是二叉树的最大深度）
+ * @param {*} root
+ */
+function maxdepth(root) {
+  //使用递归的方法 递归三部曲
+  //1. 确定递归函数的参数和返回值
+  const getdepth = function (node) {
+    //2. 确定终止条件
+    if (node === null) {
+      return 0;
+    }
+    //3. 确定单层逻辑
+    let leftdepth = getdepth(node.left);
+    let rightdepth = getdepth(node.right);
+    let depth = 1 + Math.max(leftdepth, rightdepth);
+    return depth;
+  };
+  return getdepth(root);
+}
+
+// 测试
+console.log(maxdepth(root));
--- src/core/数据结构与算法/二叉树/对称二叉树.js
+++ src/core/数据结构与算法/二叉树/对称二叉树.js
@@ -41,28 +41,28 @@
  * @param {*} root 
  * @returns 
  */
-//  function isSymmetric(root) {
-//   // 使用递归遍历左右子树 递归三部曲
-//     // 1. 确定递归的参数 root.left root.right和返回值true false 
-//     const compareNode = function(left, right) {
-//       // 2. 确定终止条件 空的情况
-//       if(left === null && right !== null || left !== null && right === null) {
-//           return false;
-//       } else if(left === null && right === null) {
-//           return true;
-//       } else if(left.val !== right.val) {
-//           return false;
-//       }
-//       // 3. 确定单层递归逻辑
-//       let outSide = compareNode(left.left, right.right);
-//       let inSide = compareNode(left.right, right.left);
-//       return outSide && inSide;
-//   }
-//   if(root === null) {
-//       return true;
-//   }
-//   return compareNode(root.left, root.right);
-//  } 
+ function isSymmetric(root) {
+  // 使用递归遍历左右子树 递归三部曲
+    // 1. 确定递归的参数 root.left root.right和返回值true false 
+    const compareNode = function(left, right) {
+      // 2. 确定终止条件 空的情况
+      if(left === null && right !== null || left !== null && right === null) {
+          return false;
+      } else if(left === null && right === null) {
+          return true;
+      } else if(left.val !== right.val) {
+          return false;
+      }
+      // 3. 确定单层递归逻辑
+      let outSide = compareNode(left.left, right.right);
+      let inSide = compareNode(left.right, right.left);
+      return outSide && inSide;
+  }
+  if(root === null) {
+      return true;
+  }
+  return compareNode(root.left, root.right);
+ } 
 
  /**
   * 迭代法（使用队列）
