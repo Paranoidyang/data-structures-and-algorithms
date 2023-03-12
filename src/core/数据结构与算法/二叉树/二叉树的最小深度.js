e07a0ffe93c880edb2eb38d5b44f7a812853b9d1
paranoidyang <paranoidyang@163.com>
2023-03-12 23:50:42 +0800

    迭代法求二叉树的最小深度

--- src/core/数据结构与算法/二叉树/二叉树的最小深度.js
+++ src/core/数据结构与算法/二叉树/二叉树的最小深度.js
@@ -31,13 +31,31 @@ const root = {
 function minDepth(root) {
   if(!root) return 0
   if(!root.left && !root.right) return 1 // 到叶子节点 返回 1
-  if(!root.left) { // 只有右节点时 递归右节点
-    return minDepth(root.right) + 1
-  }
-  if(!root.right) { // 只有左节点时 递归左节点
-    return minDepth(root.left) + 1
-  }
+  if(!root.left) return minDepth(root.right) + 1 // 只有右节点时 递归右节点
+  if(!root.right) return minDepth(root.left) + 1 // 只有左节点时 递归左节点
   return Math.min(minDepth(root.left), minDepth(root.right)) + 1
+}
+
+
+/**
+ * 迭代法
+ * @param {*} root 
+ */
+function minDepth(root) {
+  if(!root) return 0;
+    const queue = [root];
+    let dep = 0;
+    while(true) {
+        let size = queue.length;
+        dep++;
+        while(size--){
+            const node = queue.shift();
+            // 到第一个叶子节点 返回 当前深度 
+            if(!node.left && !node.right) return dep;
+            node.left && queue.push(node.left);
+            node.right && queue.push(node.right);
+        }
+    }
 }
 
 // 测试
