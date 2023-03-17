84b459712a4d6757ed876ca380189b5449dbf5f9
yangweifeng <yangweifeng@joyy.com>
2023-03-17 14:38:21 +0800

    迭代法求左叶子之和

--- src/core/数据结构与算法/二叉树/左叶子之和.js
+++ src/core/数据结构与算法/二叉树/左叶子之和.js
@@ -45,5 +45,24 @@ function sumOfLeftLeaves(root) {
   return leftValue + rightValue
 }
 
+/**
+ * 迭代法
+ * @param {*} root 
+ */
+function sumOfLeftLeaves(root) {
+  if(!root) return 0
+  const queue = [root]
+  let sum = 0
+  while(queue.length) {
+    const node = queue.shift()
+    if(node.left && node.left.left === null && node.left.right === null) {
+      sum += node.left.val
+    }
+    node.left && queue.push(node.left)
+    node.right && queue.push(node.right)
+  }
+  return sum
+}
+
 // 测试
 console.log(sumOfLeftLeaves(root))
