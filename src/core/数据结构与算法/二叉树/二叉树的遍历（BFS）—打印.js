fdefd3dd7217d86bd4c143c5f4661703bb486db7
yangweifeng <yangweifeng@joyy.com>
2023-03-20 15:23:41 +0800

    层序遍历打印二叉树

--- src/core/数据结构与算法/二叉树/二叉树的遍历（BFS）—打印.js
+++ src/core/数据结构与算法/二叉树/二叉树的遍历（BFS）—打印.js
@@ -1,6 +1,8 @@
 /**
- * 题目描述：二叉树层序遍历：按照层次的顺序，从上到下，从左到右地遍历（打印）一个二叉树的节点值
- * BFS（Breadth-First Search）：即广度优先算法
+ * 题目描述：按照层次的顺序，从上到下，从左到右地遍历（打印）一个二叉树的节点值，即层序遍历
+ * 层序遍历又称 BFS（Breadth-First Search，广度优先算法）
+ * 
+ * 技巧：借助队列实现
  */
 
 const root = {
@@ -20,20 +22,22 @@ const root = {
       val: "F"
     }
   }
-};
+}
+
 /**
- * 打印
+ * 层序遍历打印
  * @param {*} root 树的根节点
  */
-function BFSLog(root) {
-  const queue = []// 初始化队列queue
-  queue.push(root)
+function bfs(root) {
+  const queue = [root] // 初始化队列queue，并将根节点入队
   while (queue.length > 0) {
-    const top = queue[0]// 取出队头元素  
-    console.log(top.val)//访问当前节点的值
-    if (top.left) queue.push(top.left)// 如果左子树存在，左子树入队
-    if (top.right) queue.push(top.right)// 如果右子树存在，右子树入队
+    const top = queue[0] // 取出队头元素  
+    console.log(top.val) //访问当前节点的值
+    top.left && queue.push(top.left) // 如果左子树存在，左子树入队
+    top.right && queue.push(top.right) // 如果右子树存在，右子树入队
     queue.shift() // 访问完毕，队头元素出队
   }
 }
-BFS(root)
+
+// 测试
+bfs(root)
