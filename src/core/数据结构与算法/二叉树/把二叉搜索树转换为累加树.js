b53291b725bf3e42ea23e1d977e2c2a47fcb7236
paranoidyang <paranoidyang@163.com>
2023-04-25 22:08:03 +0800

    把二叉搜索树转换成为累加树.js

--- src/core/数据结构与算法/二叉树/把二叉搜索树转换为累加树.js
+++ src/core/数据结构与算法/二叉树/把二叉搜索树转换为累加树.js
@@ -1,20 +1,34 @@
 /**
- * 题目描述：给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树，使每个节点 node 的新值等于原树中大于或等于 node.val 的值之和。
+ * 题目描述：给出二叉搜索树的根节点，该树的节点值各不相同，请你将其转换为累加树，使每个节点 node 的新值等于原树中大于或等于 node.val 的值之和。
  * 
- * 二叉搜索树满足下列约束条件：
-   节点的左子树仅包含键 小于 节点键的节点。 节点的右子树仅包含键 大于 节点键的节点。 左右子树也必须是二叉搜索树。
+ * 技巧：中序遍历（左中右）是个从小到大的有序序列，那么反过来中序遍历（右中左）就是一个从大到小的序列，然后将已遍历的节点值累加即可
  */
 
 function convertBST(root) {
-  let pre = 0;
-  const ReverseInOrder = (cur) => {
-      if(cur) {
-          ReverseInOrder(cur.right);
-          cur.val += pre;
-          pre = cur.val;
-          ReverseInOrder(cur.left);
-      }
+  let sum = 0
+  const traversal = (root) => {
+    if(!root) return
+    traversal(root.right)
+    sum += root.val
+    root.val = sum
+    traversal(root.left)
   }
-  ReverseInOrder(root);
-  return root;
+  traversal(root)
+  return root
 }
+
+// 测试
+const root = {
+  val: 2,
+  left: {
+    val: 1,
+    left: null,
+    right: null
+  },
+  right: {
+    val: 3,
+    left: null,
+    right: null
+  }
+}
+console.log(convertBST(root))
