d09b7ef9a75ce22352ce662fa52e110edf0c7b6e
yangweifeng <yangweifeng@joyy.com>
2023-05-09 09:45:40 +0800

    子集

--- src/core/数据结构与算法/回溯算法/子集.js
+++ src/core/数据结构与算法/回溯算法/子集.js
@@ -1,47 +1,41 @@
 /**
  * 题目描述：给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
- * 说明：解集不能包含重复的子集。
- * 示例: 输入: nums = [1,2,3]
-  输出:
-  [
-    [3],
-    [1],
-    [2],
-    [1,2,3],
-    [1,3],
-    [2,3],
-    [1,2],
-    []
-  ]
+ * 
+ * 说明：解集不能包含重复的子集。你可以按任意顺序返回解集。
+ * 
+ * 示例1: 
+ * 输入：nums = [1,2,3]
+ * 输出：[[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]
+ * 
+ * 示例 2：
+ * 输入：nums = [0]
+ * 输出：[[],[0]]
+ * 
  */
 
-export default function () {
-
-  /**
+ /**
    * 返回该数组所有可能的子集
    * @param {*} nums 
    */
-  function subsets(nums) {
-    let res = []
-    let len = nums.length
-    let subset = []
-    function dfs(n) {
-      if (n === len) { // 遍历到叶子节点，存入结果
-        res.push(subset.slice())
-        return
-      }
-      subset.push(nums[n])// 第n层，取，dfs到下一层
-      dfs(n + 1)
-      subset.pop()// 第n层，不取，dfs到下一层
-      dfs(n + 1)
+ function subsets(nums) {
+  const path = [], result = [], len = nums.length
+  const dfs = (nums) => {
+    result.push([...path])
+    if(path.length === len) { // 终止条件可以不加
+      return
     }
-    dfs(0) // 从root开始
-    return res
+    for(let i = 0; i < nums.length; i++) {
+      path.push(nums[i])
+      dfs(nums.slice(i + 1))
+      path.pop()
+    }
   }
-  console.log(subsets([1, 2, 3]))
+  dfs(nums)
+  return result
+ }
 
-
-}
+//  测试
+ console.log(subsets([1, 2, 3]))
 
 
 
