a79bc37e8ab868cf37db6af182d71fd683f23658
paranoidyang <paranoidyang@163.com>
2023-06-24 17:23:05 +0800

    斐波那契数

--- src/core/数据结构与算法/动态规划/斐波那契数.js
+++ src/core/数据结构与算法/动态规划/斐波那契数.js
@@ -8,21 +8,40 @@
  */
 
 /**
+ * 时间复杂度O(N)，空间复杂度O(N)
 * @param {number} n
 * @return {number}
 */
-const fib = function (n) {
-  if (n < 2) return n
+function fib(n) {
+  if (n <= 1) return n
   // 初始化状态数组
-  const f = [];
+  const dp = []
   // 初始化已知值
-  f[0] = 0;
-  f[1] = 1;
+  dp[0] = 0
+  dp[1] = 1
   for (let i = 2; i <= n; i++) {
-    f[i] = f[i - 2] + f[i - 1];
+    dp[i] = dp[i - 2] + dp[i - 1]
   }
   // 返回目标值
-  return f[n];
-};
+  return dp[n]
+}
+
+/**
+ * 时间复杂度O(N)，空间复杂度O(1)
+* @param {number} n
+* @return {number}
+*/
+function fib(n) {
+  if (n <= 1) return n
+  // 初始化状态数组
+  const dp = [0, 1]
+  for (let i = 2; i <= n; i++) {
+    const sum = dp[0] + dp[1]
+    dp[0] = dp[1]
+    dp[1] = sum
+  }
+  // 返回目标值
+  return dp[1]
+}
 
 console.log(fib(0), fib(1), fib(2), fib(3), fib(4), fib(5))
