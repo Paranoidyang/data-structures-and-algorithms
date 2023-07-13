58de09f210ad7b91559d42bf92b7142679466449
paranoidyang <paranoidyang@163.com>
2023-07-13 23:57:12 +0800

    分割等和子集题目描述

--- src/core/数据结构与算法/动态规划/01背包问题.js
+++ src/core/数据结构与算法/动态规划/01背包问题.js
@@ -29,7 +29,7 @@ function testWeightBagProblem(weight, value, size) {
   for (let i = 1; i < len; i++) { // 遍历物品
     for (let j = 0; j <= size; j++) { // 遍历背包容量
       if (j < weight[i]) dp[i][j] = dp[i - 1][j];
-      else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+      else dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]); // 该递归公式由不放物品i和放物品i两个方向推出
     }
   }
 
