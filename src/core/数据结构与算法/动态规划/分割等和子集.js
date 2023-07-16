4c9f1ce23c80f0f84926b9502be2b67cd2efe0ee
paranoidyang <paranoidyang@163.com>
2023-07-16 22:29:04 +0800

    分割等和子集

--- src/core/数据结构与算法/动态规划/分割等和子集.js
+++ src/core/数据结构与算法/动态规划/分割等和子集.js
@@ -19,9 +19,23 @@
  */
 
 function canPartition(nums) {
-
+  const sum = nums.reduce((item, prev) => prev += item, 0)
+  // 和为奇数，不可能拆成和相等的两个子集
+  if (sum % 2 !== 0) return false
+  const target = sum / 2
+  // dp[j]数组定义：容量为j的背包所能放的最大价值
+  const dp = Array(sum / 2 + 1).fill(0)
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = target; nums[i] <= j; j--) {
+      dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i])
+      if (dp[j] === target) {
+        return true
+      }
+    }
+  }
 }
 
 // 测试
 console.log(canPartition([1, 5, 11, 5]))
 console.log(canPartition([1, 2, 3, 5]))
+console.log(canPartition([1, 2]))
