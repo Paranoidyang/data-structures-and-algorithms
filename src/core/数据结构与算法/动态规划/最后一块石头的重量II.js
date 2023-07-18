e2dfc3a5d2f5febb7f349e9a946a9c7e9e9c4d8d
paranoidyang <paranoidyang@163.com>
2023-07-18 22:48:15 +0800

    最后一块石头的重量II

--- src/core/数据结构与算法/动态规划/最后一块石头的重量II.js
+++ src/core/数据结构与算法/动态规划/最后一块石头的重量II.js
@@ -25,7 +25,18 @@
  */
 
 function lastStoneWeightII(stones) {
+  let sum = stones.reduce((s, n) => s + n)
 
+  let dpLen = Math.floor(sum / 2)
+  let dp = new Array(dpLen + 1).fill(0)
+
+  for (let i = 0; i < stones.length; ++i) {
+    for (let j = dpLen; j >= stones[i]; --j) {
+      dp[j] = Math.max(dp[j], dp[j - stones[i]] + stones[i])
+    }
+  }
+
+  return sum - dp[dpLen] - dp[dpLen]
 }
 
 // 测试
