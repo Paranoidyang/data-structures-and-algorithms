513b0747cc598d80668255937ea98abec3650e41
yangweifeng <yangweifeng@joyy.com>
2023-08-02 17:53:09 +0800

    数组去重

--- src/core/手写js系列/数组去重.js
+++ src/core/手写js系列/数组去重.js
@@ -2,24 +2,19 @@
  * 题目描述：数组去重
  */
 
-let array = [1, 1, '1', '1'];
-
 // 双重循环
 function unique(arr) {
-    // res用来存储结果
-    var res = [];
-    for (const item of arr) {
-        for (var i = 0, resLen = res.length; i < resLen; i++) {
-            if (item === res[i]) {
-                break;
-            }
-        }
-        // 如果item是唯一的，那么执行完循环，i等于resLen
-        if (i === resLen) {
-            res.push(item)
-        }
+  const res = []
+  for (const item of arr) {
+    for (var i = 0; i < res.length; i++) {
+      if (item === res[i]) break
     }
-    return res;
+    // 如果item是唯一的，那么执行完循环，i等于resLen
+    if (i === res.length) res.push(item)
+  }
+  return res
 }
 
-console.log(unique(array)); // [1, "1"]
+// 测试
+let array = [1, '1', 1, '1']
+console.log(unique(array)) // [1, "1"]
