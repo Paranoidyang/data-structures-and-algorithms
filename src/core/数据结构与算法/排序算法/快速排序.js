611f75d6e30dbe279c7a6a55378f183dc91c2893
paranoidyang <paranoidyang@163.com>
2023-08-03 23:54:08 +0800

    优化快速排序

--- src/core/数据结构与算法/排序算法/快速排序.js
+++ src/core/数据结构与算法/排序算法/快速排序.js
@@ -18,60 +18,33 @@
 // }
 
 // 原地版本（in-place），空间复杂度O(1)
-// 快速排序入口
 function quickSort(arr, left = 0, right = arr.length - 1) {
-  // 定义递归边界，若数组只有一个元素，则没有排序必要
-  if (arr.length > 1) {
-    // lineIndex表示下一次划分左右子数组的索引位
-    const lineIndex = partition(arr, left, right)
-    // 如果左边子数组的长度不小于1，则递归快排这个子数组
-    if (left < lineIndex - 1) {
-      // 左子数组以 lineIndex-1 为右边界
-      quickSort(arr, left, lineIndex - 1)
-    }
-    // 如果右边子数组的长度不小于1，则递归快排这个子数组
-    if (lineIndex < right) {
-      // 右子数组以 lineIndex 为左边界
-      quickSort(arr, lineIndex, right)
-    }
+  if (arr.length > 1) { // 定义递归边界，若数组只有一个元素，则没有排序必要
+    const lineIndex = partition(arr, left, right) // lineIndex表示下一次划分左右子数组的索引位
+    if (left < lineIndex - 1) quickSort(arr, left, lineIndex - 1) // 如果左边子数组的长度不小于1，则递归快排这个子数组，左子数组以 lineIndex-1 为右边界
+    if (lineIndex < right) quickSort(arr, lineIndex, right) // 如果右边子数组的长度不小于1，则递归快排这个子数组，右子数组以 lineIndex 为左边界
   }
   return arr
 }
+
 // 以基准值为轴心，划分左右子数组的过程
 function partition(arr, left, right) {
   // 基准值默认取中间位置的元素（为什么不能直接Math.floor((left + right)/ 2))？）
   // 当left和right都很大的时候，left+right可能溢出，left + (right - left) / 2就会更好一点
   let pivotValue = arr[Math.floor(left + (right - left) / 2)]
-  // 初始化左右指针
-  let i = left
-  let j = right
-  // 当左右指针不越界时，循环执行以下逻辑
-  while (i <= j) {
-    // 左指针所指元素若小于基准值，则右移左指针
-    while (arr[i] < pivotValue) {
-      i++
-    }
-    // 右指针所指元素大于基准值，则左移右指针
-    while (arr[j] > pivotValue) {
-      j--
-    }
-
-    // 若i<=j，则意味着基准值左边存在较大元素或右边存在较小元素，交换两个元素确保左右两侧有序
-    if (i <= j) {
-      swap(arr, i, j)
+  let i = left, j = right // 初始化左右指针
+  while (i <= j) { // 当左右指针不越界时，循环执行以下逻辑
+    while (arr[i] < pivotValue) i++ // 左指针所指元素若小于基准值，则右移左指针
+    while (arr[j] > pivotValue) j-- // 右指针所指元素大于基准值，则左移右指针
+    if (i <= j) { // 若i<=j，则意味着基准值左边存在较大元素或右边存在较小元素，交换两个元素确保左右两侧有序
+      [arr[i], arr[j]] = [arr[j], arr[i]]
       i++
       j--
     }
   }
-
-  // 返回左指针索引作为下一次划分左右子数组的依据
-  return i
+  return i // 返回左指针索引作为下一次划分左右子数组的依据
 }
 
-// 快速排序中使用 swap 的地方比较多，我们提取成一个独立的函数
-function swap(arr, i, j) {
-  [arr[i], arr[j]] = [arr[j], arr[i]]
-}
 console.log(quickSort([2, 1, 5, 9, 8]))
 
 console.log(quickSort([2, 3, 4, 1, 4, 1, 5]));
