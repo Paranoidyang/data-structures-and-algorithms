88c61829adc82e7ccb18537bb14524207178090d
yangweifeng <yangweifeng@joyy.com>
2023-08-10 10:41:43 +0800

    四数相加II

--- src/core/数据结构与算法/哈希表/四数相加II.js
+++ src/core/数据结构与算法/哈希表/四数相加II.js
@@ -30,29 +30,28 @@
  * @param {*} D 
  */
 function fourSumCount(A, B, C, D) {
-      let count = 0
-      const twoSumMap = new Map()
-      // 统计A和B数组元素之和，和出现的次数，放到map中
-      for (const a of A) {
-            for (const b of B) {
-                  const sum = a + b
-                  twoSumMap.set(sum, (twoSumMap.get(sum) || 0) + 1)
-            }
+  const twoSumMap = new Map()
+  let count = 0
+  for(const a of A) { // 统计A和B数组元素之和，和出现的次数，放到map中
+    for(const b of B) {
+      const sum = a + b
+      twoSumMap.set(sum, (twoSumMap.get(sum) || 0) + 1)
+    }
+  }
+  for(const c of C) { // 找到如果 0-(c+d) 在map中出现过的话，就把map中key对应的value也就是出现次数统计出来
+    for(const d of D) {
+      const sum = c + d
+      if(twoSumMap.has(0 - sum)) {
+        count += twoSumMap.get(0 - sum)
       }
-      // 找到如果 0-(c+d) 在map中出现过的话，就把map中key对应的value也就是出现次数统计出来
-      for (const c of C) {
-            for (const d of D) {
-                  const sum = c + d
-                  count += (twoSumMap.get(0 - sum) || 0)
-            }
-
-      }
-      return count
+    }
+  }
+  return count
 }
 
 // 测试
 let A = [1, 2],
-      B = [-2, -1],
-      C = [-1, 2],
-      D = [0, 2]
+    B = [-2, -1],
+    C = [-1, 2],
+    D = [0, 2]
 console.log(fourSumCount(A, B, C, D))
