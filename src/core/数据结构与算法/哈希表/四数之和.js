acc8a170b4a68c3273fc0b9ed657f0886d46ac1e
paranoidyang <paranoidyang@163.com>
2023-08-11 00:37:55 +0800

    四数之和

--- src/core/数据结构与算法/哈希表/四数之和.js
+++ src/core/数据结构与算法/哈希表/四数之和.js
@@ -7,37 +7,34 @@
  */
 
 function fourSum(nums, target) {
-  const len = nums.length;
-  if (len < 4) return [];
-  // 从小到大排序
-  nums.sort((a, b) => a - b);
-  const res = [];
+  const len = nums.length
+  nums.sort((a, b) => a - b) // 从小到大排序
+  const res = []
   for (let i = 0; i < len - 3; i++) {
-    // 去重i
-    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    if (nums[i] > target && nums[i] >= 0) break // 剪枝处理，这里使用break，统一通过最后的return返回
+    if (i > 0 && nums[i] === nums[i - 1]) continue // 对nums[i]去重
     for (let j = i + 1; j < len - 2; j++) {
-      // 去重j
-      if (j > i + 1 && nums[j] === nums[j - 1]) continue;
-      let l = j + 1, r = len - 1;
-      while (l < r) {
-        const sum = nums[i] + nums[j] + nums[l] + nums[r];
-        if (sum < target) { l++; continue }
-        if (sum > target) { r--; continue }
-        res.push([nums[i], nums[j], nums[l], nums[r]]);
-
-        // 去重逻辑应该放在找到一个四元组之后，对nums[left]和nums[right]去重
-        // 以下代码可以简化成：
-        // while(l < r && nums[l] === nums[++l]);
-        // while(l < r && nums[r] === nums[--r]);
-        while (l < r && nums[l] === nums[l + 1]) l++;
-        while (l < r && nums[r] === nums[r - 1]) r--;
-        // 找到答案时，双指针同时收缩
-        l++
-        r
+      if (nums[i] + nums[j] > target && nums[i] + nums[j] >= 0) break // 2级剪枝处理
+      if (j > i + 1 && nums[j] === nums[j - 1]) continue // 对nums[j]去重
+      let left = j + 1
+      let right = len - 1
+      while (left < right) {
+        const sum = nums[i] + nums[j] + nums[left] + nums[right]
+        if (sum < target) left++
+        else if (sum > target) right--
+        else {
+          res.push([nums[i], nums[j], nums[left], nums[right]])
+          // 去重逻辑应该放在找到一个四元组之后，对nums[left]和nums[right]去重
+          while (left < right && nums[left] === nums[left + 1]) left++
+          while (left < right && nums[right] === nums[right - 1]) right--
+          // 找到答案时，双指针同时收缩
+          left++
+          right--
+        }
       }
     }
   }
-  return res;
+  return res
 }
 
 // 测试
