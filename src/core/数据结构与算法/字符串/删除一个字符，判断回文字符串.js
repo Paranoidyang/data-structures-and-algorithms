530001fb1f79857b3da4156d2c34cee48aa067b2
paranoidyang <paranoidyang@163.com>
2023-08-16 21:58:11 +0800

    双指针法判断是否是回文字符串

--- src/core/数据结构与算法/字符串/删除一个字符，判断回文字符串.js
+++ src/core/数据结构与算法/字符串/删除一个字符，判断回文字符串.js
@@ -9,49 +9,45 @@
     解释: 你可以删除c字符。
     注意: 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。
  */
-export default function () {
-  /**
+
+/**
    * 最多删除一个字符。判断是否能成为回文字符串
    * @param {*} str 
    */
-  function validPalindrome(str) {
-    const len = str.length
-    // i、j分别为左右指针
-    let i = 0, j = len - 1
-    // 当左右指针均满足对称时，一起向中间前进
-    while (i < j && str[i] === str[j]) {
-      i++
-      j--
-    }
-    // 尝试判断跳过左指针元素后字符串是否回文
-    if (isPalindrome(i++, j)) {
-      return true
-    }
-    // 尝试判断跳过右指针元素后字符串是否回文
-    if (isPalindrome(i, j--)) {
-      return true
-    }
-
-    // 工具方法，用于判断字符串是否回文
-    function isPalindrome(st, ed) {
-      while (st < ed) {
-        if (str[st] === str[ed]) {
-          st++
-          ed--
-        } else {
-          return false
-        }
-      }
-      return true
-    }
-    // 默认返回 false
-    return false
+function validPalindrome(str) {
+  const len = str.length
+  // i、j分别为左右指针
+  let i = 0, j = len - 1
+  // 当左右指针均满足对称时，一起向中间前进
+  while (i < j && str[i] === str[j]) {
+    i++
+    j--
+  }
+  // 尝试判断跳过左指针元素后字符串是否回文
+  if (isPalindrome(i++, j)) {
+    return true
+  }
+  // 尝试判断跳过右指针元素后字符串是否回文
+  if (isPalindrome(i, j--)) {
+    return true
   }
 
-
-  console.log(validPalindrome('aaaa'))
-
-
-
+  // 工具方法，用于判断字符串是否回文
+  function isPalindrome(start, end) {
+    while (start < end) {
+      if (str[start] === str[end]) {
+        start++
+        end--
+      } else {
+        return false
+      }
+    }
+    return true
+  }
+  // 默认返回 false
+  return false
 }
+
+
+console.log(validPalindrome('aaaa'))
 
--- src/core/数据结构与算法/字符串/判断回文字符串.js
+++ src/core/数据结构与算法/字符串/判断回文字符串.js
@@ -2,38 +2,38 @@
  * 题目描述：回文字符串就是就是正着读和倒着读都一模一样的字符串，如'yessey'
  * 回文字符串还有另一个特性：如果从中间位置“劈开”，那么两边的两个子串在内容上是完全对称的。
  */
-export default function () {
-  /**
+
+/**
    * 判断一个字符串是否是回文字符串
    * @param {*} str 
    * @returns 
    */
-  // 用反转字符串判断
-  function isPalindrome1(str) {
-    // 反转字符串
-    let reversedStr = str.split('').reverse().join('')
-    return str === reversedStr
-  }
-
-  /**
-   * 判断一个字符串是否是回文字符串
-   * @param {*} str 
-   * @returns 
-   */
-  // 用对称性判断
-  function isPalindrome2(str) {
-    // 缓存字符串的长度
-    const len = str.length
-    // 遍历前半部分，判断和后半部分是否对称
-    for (let i = 0; i < len / 2; i++) {
-      if (str[i] !== str[len - i - 1]) {
-        return false
-      }
-    }
-    return true
-  }
-  console.log(isPalindrome1('abba'))
-  console.log(isPalindrome2('abba'))
-
+// 用反转字符串判断
+function isPalindrome1(str) {
+  // 反转字符串
+  let reversedStr = str.split('').reverse().join('')
+  return str === reversedStr
 }
+
+/**
+ * 双指针
+ * 判断一个字符串是否是回文字符串
+ * @param {*} str 
+ * @returns 
+ */
+// 用对称性判断
+function isPalindrome2(str) {
+  let start = 0, end = str.length - 1
+  while (start < end) {
+    if (str[start] === str[end]) {
+      start++
+      end--
+    } else {
+      return false
+    }
+  }
+  return true
+}
+console.log(isPalindrome1('abba'))
+console.log(isPalindrome2('abba'))
 
