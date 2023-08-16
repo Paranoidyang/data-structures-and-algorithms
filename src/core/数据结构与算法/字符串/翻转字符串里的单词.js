d5896f39b9584d2b7fc86187ac4ad872e8fca365
paranoidyang <paranoidyang@163.com>
2023-08-16 21:36:31 +0800

    翻转字符串里的单词

--- src/core/数据结构与算法/字符串/翻转字符串里的单词.js
+++ src/core/数据结构与算法/字符串/翻转字符串里的单词.js
@@ -24,54 +24,44 @@
  * 翻转字符串中的每个单词
  */
 function reverseWords(s) {
-  // 字符串转数组
-  const strArr = Array.from(s);
-  // 移除多余空格
-  removeExtraSpaces(strArr);
-  // 翻转
-  reverse(strArr, 0, strArr.length - 1);
-
-  let start = 0;
-
+  const strArr = Array.from(s) // 字符串转数组
+  removeExtraSpaces(strArr) // 移除多余空格
+  reverse(strArr, 0, strArr.length - 1) // 翻转整个字符串
+  let start = 0
   for (let i = 0; i <= strArr.length; i++) {
-    if (strArr[i] === ' ' || i === strArr.length) {
-      // 翻转单词
-      reverse(strArr, start, i - 1);
-      start = i + 1;
+    if (strArr[i] === ' ' || i === strArr.length) { // 遇到空格或者串尾，说明一个单词结束，进行翻转
+      reverse(strArr, start, i - 1) // 翻转单词
+      start = i + 1
     }
   }
-
-  return strArr.join('');
+  return strArr.join('')
 }
 
-// 删除多余空格
+// 快慢指针
+// 去除所有空格并在相邻单词之间添加空格
 function removeExtraSpaces(strArr) {
-  let slowIndex = 0;
-  let fastIndex = 0;
-
-  while (fastIndex < strArr.length) {
-    // 移除开始位置和重复的空格
-    if (strArr[fastIndex] === ' ' && (fastIndex === 0 || strArr[fastIndex - 1] === ' ')) {
-      fastIndex++;
-    } else {
-      strArr[slowIndex++] = strArr[fastIndex++];
+  let slow = 0
+  const len = strArr.length
+  for (let fast = 0; fast < len; fast++) {
+    if (strArr[fast] !== ' ') { // 遇到非空格就处理，即删除所有空格
+      if (slow !== 0) strArr[slow++] = ' ' // 手动控制空格，给单词之间添加空格。slow != 0说明不是第一个单词，需要在单词前添加空格。
+      while (fast < len && strArr[fast] != ' ') { // 补上该单词，遇到空格说明单词结束
+        strArr[slow++] = strArr[fast++]
+      }
     }
   }
-
-  // 移除末尾空格
-  strArr.length = strArr[slowIndex - 1] === ' ' ? slowIndex - 1 : slowIndex;
+  strArr.length = slow // slow的大小即为去除多余空格后的大小
 }
 
 // 翻转从 start 到 end 的字符
 function reverse(strArr, start, end) {
-  let left = start;
-  let right = end;
-
+  let left = start
+  let right = end
   while (left < right) {
     // 交换
-    [strArr[left], strArr[right]] = [strArr[right], strArr[left]];
-    left++;
-    right--;
+    [strArr[left], strArr[right]] = [strArr[right], strArr[left]]
+    left++
+    right--
   }
 }
 
